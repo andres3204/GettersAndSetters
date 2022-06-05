85f937c312094187ab1dd3102371e57e1774dc73
Andres Enrique Jiménez Mogollón <andresejimenez@hotmail.com>
2022-06-05 12:10:09 -0500

    Add project files.

---
+++ GettersAndSetters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GettersAndSetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie avengers = new Movie(" The Avengers", " Joss Whedon", " PG-13");
+            Movie shrek = new Movie(" Shrek", " Adam Adamson", " PG");
+
+            Console.WriteLine(avengers.title);
+            Console.WriteLine(avengers.director);
+            Console.WriteLine(avengers.Rating);
+            Console.WriteLine("----");
+            Console.WriteLine(shrek.title);
+            Console.WriteLine(shrek.director);
+            Console.WriteLine(shrek.Rating);
+
+            Console.ReadLine();
+        }
+    }
+}
